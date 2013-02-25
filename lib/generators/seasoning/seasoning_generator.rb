37b881e7827208679a71d69ae23be5c8478fccfc
Kori Roys <kori@koriroys.com>
2013-02-25 16:13:57 -0600

    update to reflect SecureRandom changes

--- lib/generators/seasoning/seasoning_generator.rb
+++ lib/generators/seasoning/seasoning_generator.rb
@@ -1,15 +1,14 @@
 require 'rails/generators/base'
 
 class SeasoningGenerator < Rails::Generators::Base #:nodoc:
-  require 'active_support/secure_random'
 
   class_option :devise, :type => :boolean, :required => false, :aliases => "-d",
                         :default => false, :desc => "Also seasons devise"
 
   def season_app
-    gsub_file 'config/initializers/secret_token.rb', /config.secret_token = \'[a-z0-9]+\'/, "config.secret_token = '#{ActiveSupport::SecureRandom.hex(64)}'"
+    gsub_file 'config/initializers/secret_token.rb', /config.secret_token = \'[a-z0-9]+\'/, "config.secret_token = '#{SecureRandom.hex(64)}'"
     if options[:devise]
-      gsub_file 'config/initializers/devise.rb', /config.pepper = \"([a-z0-9]+)\"/, "config.pepper = \"#{ActiveSupport::SecureRandom.hex(64)}\""
+      gsub_file 'config/initializers/devise.rb', /config.pepper = \"([a-z0-9]+)\"/, "config.pepper = \"#{SecureRandom.hex(64)}\""
     end
   end
 end
